beb28dcc237b59f2e1922d943a3a8cc9dce1ee09
Snitzelberg <48810985+Snitzelberg@users.noreply.github.com>
2021-04-14 00:32:10 -0300

    Delete filtro.py

--- filtro.py
+++
@@ -1,9 +0,0 @@
-def filtro(command,deletion):
-    lista = list(command.split(" "))
-    stopwords = ["translate","plot","ratings"]
-    stopwords.append(deletion)
-    for word in list(lista):
-        if word in stopwords:
-            lista.remove(word)
-    comando = ' '.join(str(e) for e in lista)
-    return comando
